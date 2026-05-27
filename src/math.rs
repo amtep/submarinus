ffe82656d3347b37c820401378d60cc898dd312a
Richard Braakman <shuttlegit@gmail.com>
2026-05-27 17:24:47 +0300

    exact collisions

--- src/math.rs
+++ src/math.rs
@@ -25,31 +25,75 @@ pub fn get_triangles2d(mesh: &Mesh, transform: &Transform) -> Vec<Triangle2d> {
 /// Check whether two sets of triangles have any overlap with each other.
 pub fn triangles2d_overlap(triangles1: &[Triangle2d], triangles2: &[Triangle2d]) -> bool {
     for triangle1 in triangles1 {
-        for triangle2 in triangles2 {
-            if triangle2d_contains_point(triangle1, triangle2.vertices[0])
-                || triangle2d_contains_point(triangle1, triangle2.vertices[1])
-                || triangle2d_contains_point(triangle1, triangle2.vertices[2])
-            {
-                return true;
+        for vertex1 in 0..3 {
+            let s1 = Segment2d::new(
+                triangle1.vertices[vertex1],
+                triangle1.vertices[(vertex1 + 1) % 3],
+            );
+            for triangle2 in triangles2 {
+                for vertex2 in 0..3 {
+                    let s2 = Segment2d::new(
+                        triangle2.vertices[vertex2],
+                        triangle2.vertices[(vertex2 + 1) % 3],
+                    );
+                    if segment2d_intersect(&s1, &s2) {
+                        return true;
+                    }
+                }
             }
         }
     }
     false
 }
 
-/// Check whether a point is inside a [`Triangle2d`]
-pub fn triangle2d_contains_point(triangle: &Triangle2d, point: Vec2) -> bool {
-    // Algorithm from https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
-    fn sign(p1: Vec2, p2: Vec2, p3: Vec2) -> f32 {
-        (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y)
+/// Returns whether this [`Segment2d`] intersects another.
+/// Adapted from bevy PR#20300
+pub fn segment2d_intersect(seg1: &Segment2d, seg2: &Segment2d) -> bool {
+    let p = seg1.point1();
+    let q = seg2.point1();
+    let r = seg1.scaled_direction();
+    let s = seg2.scaled_direction();
+
+    let pq = q - p;
+    let pq_cross_r = pq.perp_dot(r);
+    let pq_cross_s = pq.perp_dot(s);
+    let r_cross_s = r.perp_dot(s);
+
+    if r_cross_s != 0.0 {
+        // non parallel
+        let t = pq_cross_s / r_cross_s;
+        let u = pq_cross_r / r_cross_s;
+        let t_in_range = (0.0..=1.0).contains(&t);
+        let u_in_range = (0.0..=1.0).contains(&u);
+        t_in_range && u_in_range
+    } else if pq_cross_r == 0.0 || pq_cross_s == 0.0 {
+        // collinear
+        let r_len2 = r.length_squared();
+        let s_len2 = s.length_squared();
+        match (r_len2 == 0.0, s_len2 == 0.0) {
+            // point point
+            (true, true) => pq.length_squared() == 0.0,
+            // segment point
+            (false, true) if pq_cross_r == 0.0 => {
+                let t = pq.dot(r) / r_len2;
+                (0.0..=1.0).contains(&t)
+            }
+            // point segment
+            (true, false) if pq_cross_s == 0.0 => {
+                let t = -pq.dot(s) / s_len2;
+                (0.0..=1.0).contains(&t)
+            }
+            // segment segment
+            (false, false) => {
+                let t0 = pq.dot(r) / r_len2;
+                let t1 = t0 + s.dot(r) / r_len2;
+                let (t_min, t_max) = if t0 < t1 { (t0, t1) } else { (t1, t0) };
+                t_max >= 0.0 && t_min <= 1.0
+            }
+            _ => false,
+        }
+    } else {
+        // parallel
+        false
     }
-
-    let d1 = sign(point, triangle.vertices[0], triangle.vertices[1]);
-    let d2 = sign(point, triangle.vertices[1], triangle.vertices[2]);
-    let d3 = sign(point, triangle.vertices[2], triangle.vertices[0]);
-
-    let has_neg = d1 < 0.0 || d2 < 0.0 || d3 < 0.0;
-    let has_pos = d1 > 0.0 || d2 > 0.0 || d3 > 0.0;
-
-    !(has_neg && has_pos)
 }
